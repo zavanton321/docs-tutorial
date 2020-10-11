3794360976775cccbc3eda3b3a3f7a43eb4ad759
zavanton <zavanton123@gmail.com>
2020-10-11 20:26:34 +0300

    detail, result and other views added

--- student/models.py
+++ student/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
-
 # Create your models here.
+from django.urls import reverse
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'question_id': self.pk})
 
     def __str__(self):
         return self.question_text
@@ -17,5 +22,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
